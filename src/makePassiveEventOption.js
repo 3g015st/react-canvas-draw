65f4fbbf25d78922b4712b973e2f52555bd1f0f0
bεn <dev.embidiel@gmail.com>
2024-04-15 19:52:21 +0800

    Update makePassiveEventOption.js

--- src/makePassiveEventOption.js
+++ src/makePassiveEventOption.js
@@ -1,17 +1,17 @@
 // Determines if the browser supprots passive events
 // https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener#Safely_detecting_option_support
-let passiveSupported = false;
+let passiveSupported = true;
 try {
   const options = {
     get passive() {
-      passiveSupported = false;
-      return false;
+      passiveSupported = true;
+      return true;
     }
   };
   window.addEventListener("test", options, options);
   window.removeEventListener("test", options, options);
 } catch(e) {
-  passiveSupported = false;
+  passiveSupported = true;
 }
 
 export default function makePassiveEventOption(passive) {
